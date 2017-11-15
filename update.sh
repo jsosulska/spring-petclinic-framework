ed2f6abab02d29f402a418d9831d760b50874a4e
Benjamin Stein <ben@liatrio.com>
2017-11-14 19:42:58 -0800

    added space for formatting

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 eval $(minishift oc-env)
 eval $(minishift docker-env)
+
 docker login -u developer -p $(oc whoami -t) $(minishift openshift registry)
 mvn clean install -DskipTests
 docker build -t $(minishift openshift registry)/myproject/petclinic:latest -f dockerfiles/Jboss .
